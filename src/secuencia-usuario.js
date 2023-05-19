f015e6a618d682335e4ec2cb66101eb90843d1fb
Lucas <lucasdgonzalez@outlook.com>
2023-05-19 17:27:18 -0300

    Completa la secuencia del usuario

--- src/secuencia-usuario.js
+++ src/secuencia-usuario.js
@@ -1,16 +1,48 @@
 function guardarColorUsuario(color) {
   listaSecuenciaUsuario.push(color);
-  console.log("La secuencia del usuario:", listaSecuenciaUsuario); 
-  // Estos console.log son únicamente para comprobar que se guarde correctamente el color
-  console.log("La secuencia de la máquina:", listaSecuenciaMaquina); 
-  // Estos console.log son únicamente para comprobar que se guarde correctamente el color
 }
 
-Object.values(coloresTablero).forEach((color) => {
-  color.addEventListener("click", () => {
-    if(indicadorDeTurnos === "usuario"){
-      guardarColorUsuario(color);
+function mostrarColorElegidoUsuario(cuadroElegido) {
+  if (cuadroElegido.classList.contains("cuadro-verde")) {
+    cuadroElegido.id = "verde-iluminado";
+  } else if (cuadroElegido.classList.contains("cuadro-rojo")) {
+    cuadroElegido.id = "rojo-iluminado";
+  } else if (cuadroElegido.classList.contains("cuadro-amarillo")) {
+    cuadroElegido.id = "amarillo-iluminado";
+  } else if (cuadroElegido.classList.contains("cuadro-azul")) {
+    cuadroElegido.id = "azul-iluminado";
+  }
+
+  setTimeout(() => {
+    cuadroElegido.id = "";
+  }, 0500);
+}
+
+function validarColorCorrecto() {
+  let ultimoColorUsuario = listaSecuenciaUsuario[listaSecuenciaUsuario.length - 1];
+  let ultimoColorMaquina = listaSecuenciaMaquina[listaSecuenciaUsuario.length - 1];
+
+  if (ultimoColorMaquina === ultimoColorUsuario) {
+    if (listaSecuenciaUsuario.length === listaSecuenciaMaquina.length) {
+      listaSecuenciaUsuario = [];
+      gestionarRondas();
+
+      setTimeout(function () {
+        secuenciaMaquina();
+      }, 600);
+    }
+  } else {
+    indicadorDeTurnos = "";
+    terminarJuego();
+  }
+}
+
+Object.values(cuadrosTablero).forEach((cuadro) => {
+  cuadro.addEventListener("click", () => {
+    if (indicadorDeTurnos === "usuario") {
+      guardarColorUsuario(cuadro);
+      mostrarColorElegidoUsuario(cuadro);
+      validarColorCorrecto();
     }
   });
 });
-
